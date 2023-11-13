94ac56ce3a1ec2c5cd0a93f12441a809af32afb6
Mohith <mohithrshetty03@gmail.com>
2023-11-13 19:31:35 +0530

    resume

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Resume from './components/Resume';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Resume/>
+    </div>
+  );
+}
+
+export default App;
